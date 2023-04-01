fc958b6c35e685617485f7ebbb048502aa415ebb
kaptumg <kaptumgladys68@gmail.com>
2023-04-01 04:35:25 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,7 +2,10 @@
 """Create a class and call it square"""
 
 class Square:
+    """this class represents a square"""
 
     def __init__(self, size):
-        """defines size"""
+        """initializes size
+            Args:
+                size: size of the square"""
         self.size = size
